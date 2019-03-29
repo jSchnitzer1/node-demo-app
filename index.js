0b6d50857c3c81796143a592da6c0df2ac5d73d7
Khaled Jendi <khaledj@kth.se>
2019-03-29 23:48:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('!Hello 55 World1!');
+  res.send('!Hello v2 World1!');
 });
 
 var server = app.listen(3000, function () {
